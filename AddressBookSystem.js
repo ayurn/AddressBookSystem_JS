d63465405c9268137d3b3e9230c265ee94e72a31
ayurn <65171090+ayurn@users.noreply.github.com>
2021-06-10 15:06:52 +0530

    Added : Created addressBook and adding one contact

--- AddressBookSystem.js
+++ AddressBookSystem.js
@@ -1,1 +1,32 @@
 console.log("Welcome to address book system")
+
+var prompt = require('prompt-sync')();
+class Contact {
+
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phoneNumber;
+    email;
+
+    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    toString() {
+        return "First name: " + this.firstName + "\nLast name: " + this.lastName + "\nAddress: " + this.address + "\nCity: " + this.city + "\nState: " + this.state + "\nZip: " + this.zip + "\nPhone number: " + this.phoneNumber + "\nEmail: " + this.email;
+    }
+}
+
+let contact = new Contact("Ayur", "Ninawe", "AMtSQ", "Amravati", "Maharashtra", "781036", "7777744499", "ayur@gmail.com");
+console.log(contact.toString());
